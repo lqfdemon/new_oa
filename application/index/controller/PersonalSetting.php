fa108dbb6db248342204fd5875c324c54a37b70e
lqfdemion <lqfdemion@163.com>
2018-05-31 00:15:11 +0800

    添加个人设置 密码修改

---
+++ application/index/controller/PersonalSetting.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use think\View;
+use think\Db;
+use think\Session;
+use think\Loader;
+use think\Log;
+
+use app\index\model\User;
+
+class PersonalSetting extends CommonController
+{
+	public function change_password(){
+		$view=new View();
+		return $view->fetch('change_password');
+	}
+	public function save_password($old_psw,$new_psw,$confirm_psw){
+		$user_id = Session::get('id');
+		$user = User::where('id',$user_id)->find();
+        if($user['password']==md5($old_psw)){
+        	if($new_psw !== $confirm_psw){
+        		$this->error('两次密码输入不一致');
+        	}else{
+        		$user['password'] = md5($new_psw);
+        		$user->save();
+        		$this->success('修改成功');
+        	}
+        }else{
+        	$this->error('原始密码错误');
+        }
+	}
+}
